9856ad7bd2d527b64cdb659a1919d73e994795c5
Robbie Watt <rw1812@ic.ac.uk>
2018-04-27 11:12:00 +0100

    Added a fix to the Boys function. Used an extra taylor expand. Probabily isn't the best method for now though.

--- src/Functions.cpp
+++ src/Functions.cpp
@@ -74,7 +74,7 @@ double Functions::ErrorFunction(double x)
 double Functions::BoysFunction(int v, double u)
 {
 	double result(0);
-	if(u > 0.025)
+	if(u > 0.25) // Check this number
 	{
 		double factor1 = Factorial(2.0 * v) / (2.0 *  Factorial(v));
 		double factor2 = std::sqrt(Constants::pi) * ErrorFunction(std::sqrt(u))
@@ -88,7 +88,8 @@ double Functions::BoysFunction(int v, double u)
 		result = factor1 * ( factor2 - std::exp(-1.0 * u) * sum);
 	} else
 	{
-		result = (1.0 / (2.0 * v + 1.0)) - (u / (2.0 * v + 3.0)) + (u * u/(4.0 * v + 5.0));
+		result = (1.0 / (2.0 * v + 1.0)) - (u / (2.0 * v + 3.0)) + (u * u/(4.0 * v + 5.0)) 
+				 - (u * u * u/(12.0 * v + 42.0));
 	}
 	return result;
 }
